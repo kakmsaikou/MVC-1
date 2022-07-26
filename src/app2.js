fd7f34837a7fc21e2992e6ff03b36adb4e12df3c
Tacitus <kakmsaikou@163.com>
2022-07-26 18:27:01 +0800

    完成了 EventBus 的继承

--- src/app2.js
+++ src/app2.js
@@ -1,8 +1,9 @@
 import "./app2.css";
 import $ from "jquery";
 import Model from "./base/Model";
+import View from "./base/View.js";
+import EventBus from "./base/EventBus";
 
-const eventBus = $({})
 
 const localKey = "app2.index"
 const m = new Model({
@@ -11,15 +12,19 @@ const m = new Model({
     },
     update: function(data){
         Object.assign(m.data, data)
-        eventBus.trigger('m:updated')
+        m.trigger('m:updated')
         localStorage.setItem(localKey, data.index)
     }
 })
 
-const view = {
-    el: null,
-    html: (index)=>{
-        return  `
+
+
+const init = (el) => {
+    new View({
+        el: el,
+        data: m.data,
+        html: (index)=>{
+            return  `
       <div>
         <ol class="tab-bar">
           <li class="${index === 0 ? 'selected' : ''}" data-index='0'>1</li>
@@ -30,39 +35,23 @@ const view = {
           <li class="${index === 1 ? 'active' : ''}">内容2</li>
         </ol>
       </div>`
-    },
-    render(index) {
-        if (view.el.children.length !== 0) {
-            view.el.empty()
-        }
-        $(view.html(index)).appendTo(view.el)
-    },
-    init(container) {
-        view.el = $(container)
-        view.render(m.data.index) // view = render(data)
-        view.autoBindEvents()
-        eventBus.on('m:updated',()=>{
-            view.render(m.data.index)
-        })
-    },
-    events:{
-        'click .tab-bar li': 'x',
-    },
-    x(e){
-        const index = parseInt(e.currentTarget.dataset.index)
-        m.update({index: index})
-    },
-    autoBindEvents() {
-        for(let key in view.events){
-            const value = view[view.events[key]]
-            // 得到空格的位置
-            const spaceIndex = key.indexOf(' ')
-            const part1 = key.slice(0, spaceIndex)
-            const part2 = key.slice(spaceIndex + 1)
-            view.el.on(part1,part2,value)
-        }
-    }
+        },
+        render(data) {
+            const index = data.index
+            if (this.el.children.length !== 0) {
+                this.el.empty()
+            }
+            $(this.html(index)).appendTo(this.el)
+        },
+        events:{
+            'click .tab-bar li': 'x',
+        },
+        x(e){
+            const index = parseInt(e.currentTarget.dataset.index)
+            m.update({index: index})
+        },
+    })
 }
 
-export default view
+export default init
 
