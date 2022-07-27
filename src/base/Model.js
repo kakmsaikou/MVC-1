967bc7eb6fefddc29de2d984f279fd26d23d9882
Tacitus <kakmsaikou@163.com>
2022-07-27 20:06:54 +0800

    使用了vue

--- src/base/Model.js
+++ src/base/Model.js
@@ -1,27 +1,22 @@
-import EventBus from "./EventBus";
+import EventBus from "./EventBus.js";
 
-class Model extends EventBus{
-    constructor(options) {
-        super() // 调用 EventBus#constructor()
-        const keys = ['data', 'create', 'delete', 'update', 'get']
-        keys.forEach((key )=> {
-            if(key in options){
-                this[key] = options[key]
-            }
-        })
-    }
-    create(){
-        console && console.error && console.error('你还没有实现 create()')
-    }
-    delete(){
-        console && console.error && console.error('你还没有实现 delete()')
-    }
-    update(){
-        console && console.error && console.error('你还没有实现 update()')
-    }
-    get(){
-        console && console.error && console.error('你还没有实现 get()')
-    }
+class Model extends EventBus {
+  constructor(options) {
+    super();
+    Object.assign(this, options);
+  }
+  create() {
+    console && console.error && console.error("你还没有实现 create()");
+  }
+  delete() {
+    console && console.error && console.error("你还没有实现 delete()");
+  }
+  update() {
+    console && console.error && console.error("你还没有实现 update()");
+  }
+  get() {
+    console && console.error && console.error("你还没有实现 get()");
+  }
 }
 
-export default  Model
+export default Model;
